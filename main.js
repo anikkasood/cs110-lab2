7f23b0e63781f18d930b344f5f2a558c330b49f4
Kelsey Musolf <kelsey@musolf.net>
2024-04-26 20:14:51 -0700

    FINISHED TWO PLAYER GAME! yay
    Co-authored-by: Anika <anikkasood@users.noreply.github.com>

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ console.log("script loaded")
 var turnlabel = document.getElementById("player-turn");
 turnlabel.innerHTML= "X";
 
+var playerNum = document.getElementById("playerNumLabel");
+playerNum.innerHTML = "Two Players";
+
 var xscore = 0; 
 var yscore = 0; 
 
@@ -26,10 +29,10 @@ var yscore_label = document.getElementById("y-score");
 var printWinner = document.getElementById("outputWinner");
 
 istaken = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
 
 function play(selection) {
-    var turnlabel = document.getElementById("player-turn");
     
     var item = items[selection - 1];
    
@@ -41,16 +44,17 @@ function play(selection) {
             //set the users selection to o in the list
             item.innerHTML = "O";
             item.style.backgroundColor = "#ff8080";
+            item.style.height = "159px";
             istaken[selection - 1] = 2;
             player += 1;
             turnlabel.innerHTML= "X";
-            //winner = isWinner();
 
         }
         else{
             //player 1 == x
             item.innerHTML = "X";
             item.style.backgroundColor = "#ff8080";
+            item.style.height = "159px";
             istaken[selection - 1] = 1;
             player += 1;
             turnlabel.innerHTML= "O";
@@ -64,11 +68,18 @@ function play(selection) {
         if (winner === "x") {
             xscore += 1; 
             xscore_label.innerHTML = xscore;
-            reset();
+            for (var i = 0; i < 9; i++) {
+                istaken[i] = 3;
+            }
+            
+            
         } else if (winner === "y") {
             yscore += 1; 
             yscore_label.innerHTML = yscore;
-            reset();
+            for (var i = 0; i < 9; i++) {
+                istaken[i] = 3;
+            }
+            
         }
 
 
@@ -111,6 +122,9 @@ function isWinner(){
         // O wins vertically
         outputWinner.innerHTML = "O Wins!";
         return "y";
+    } else if(full()){ // if the grid is full check if there's a tie
+        outputWinner.innerHTML = "It's a tie!";
+        return "b";
     } else {
         outputWinner.innerHTML = "";
         return "a";
@@ -126,6 +140,55 @@ function reset(){
         items[i].style.backgroundColor = "pink";
     }
 
+    player = 1;
+    //your turn to output x
+    turnlabel.innerHTML= "X";
+
+    yscore = 0; 
+    yscore_label.innerHTML = yscore;
+
+    xscore = 0; 
+    xscore_label.innerHTML = xscore;
+
     outputWinner.innerHTML = "";
 
 }
+
+function newGame(){
+
+    //reset background
+    for (var i = 0; i < 9; i++) {
+        istaken[i] = 0;
+        items[i].innerHTML = "";
+        items[i].style.backgroundColor = "pink";
+    }
+
+    //reset board
+    for (var i = 0; i < 9; i++) {
+        istaken[i] = 0;
+    }
+
+    //make x start
+    player = 1;
+    turnlabel.innerHTML= "X";
+
+    //clear winner
+    outputWinner.innerHTML = "";
+
+}
+
+
+function full(){
+    var count = 0;
+    for (var i = 0; i < 9; i++) {
+        if(istaken[i]===0){
+            count += 1;
+        }
+    }
+    if (count === 0) { // there are no 0s, so the grid is full
+        return true;
+    }
+    else return false;
+
+}
+//<|:)->-<
